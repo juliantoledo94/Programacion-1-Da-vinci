c8eecd803ea3c3e5a051d9a35e553cc735d0abe6
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2022-10-17 14:25:39 -0300

    Update index.js

--- programacion 1/p1-parcial-1-dw[mtn]2[abc]/index.js
+++ programacion 1/p1-parcial-1-dw[mtn]2[abc]/index.js
@@ -1,24 +1,5 @@
 
-/*
-Cualquier faltante de los requisitos que se presentan a continuación, hará que la entrega se considere desaprobada, aunque haya hecho cosas no solicitadas o del punto siguiente. Esta condición es mínima e innegociable.
 
-Al hacer clic en el botón "Cargar nuevo disco" se debe disparar la función Cargar que solicita al usuario la siguiente información de un disco:
-Nombre del disco.
-Autor o banda del disco.
-Código numérico único del disco:
-Si el usuario ingresa un código numérico que ya fue cargado, se deberá pedir otro código (hasta ingresar uno que no haya sido utilizado anteriormente).
-Todas las pistas del disco, donde cada pista tiene un nombre y una duración:
-La cantidad de pistas a ingresar NO debe ser fija.
-No se debe preguntar cuántas pistas hay que cargar, el dato se desconoce.
-Al finalizar la carga de una pista, se debe confirmar si se desea ingresar otra más.
-Cuando el usuario decide dejar de cargar pistas, finaliza la acción de la función.
-En todo caso debe validarse que los datos ingresados sean válidos:
-El nombre del disco, autor/banda y nombre de la pista no pueden quedar vacíos.
-El código numérico único del disco no puede ser menor a 1, ni mayor a 999.
-La duración de cada pista debe estar entre 0 y 7200 (segundos) inclusive.
-Al hacer clic en el botón "Mostrar discos", la función Mostrar debe mostrar cada disco con su nombre, autor/banda, código único y todas las pistas con sus respectivos datos: nombre y duración.
-Hay que destacar visualmente con rojo las duraciones mayores a 180.
-*/
 
 class Disco{
     #nombre;
@@ -67,12 +48,6 @@ class Disco{
 
 
 
-    validarCodigo(codigo){
-
-        let valido = false;
-       
-
-    }
 }
 
 class Pista{
@@ -84,7 +59,7 @@ class Pista{
         this.#duracion = duracion;
     }
 
-    set setNombre(nombre){
+    set setNombrePista(nombre){
         this.#nombre = nombre;
         
     }
@@ -96,9 +71,6 @@ class Pista{
 
 
 
-function Cargar(){
-
-}
 
 function validarStrings(mensaje){
 
@@ -112,6 +84,7 @@ function validarStrings(mensaje){
         }
         else{
             alert("No esta ingresando un string");
+            mensaje=prompt("ingrese nuevamente pero sin numeros")
             flag=false;
             
         }
@@ -119,4 +92,37 @@ function validarStrings(mensaje){
 
 }
 
+function Cargar(){
+    
+    disco.setNombre=validarStrings(prompt("ingrese nombre del disco:"));
+    disco.setAutor=prompt("ingrese autor")
+    disco.setCodigo=parseInt(prompt("ingrese codigo"));
 
+    pista.setNombrePista=prompt("ingrese nombre de la pista: ");
+    pista.setDuracion=parseInt(prompt("ingrese duracion en minutos"));
+
+    disco.agregarPista(pista);
+
+    lista_discos.push(disco);
+
+    return lista_discos;
+
+}
+
+
+function Mostrar(){
+    
+}
+
+
+// let disco = new Disco(setNombre=prompt("ingrese nombre del disco:"),setAutor=prompt("ingrese autor"),setCodigo=parseInt(prompt("ingrese codigo")));
+
+// let pista = new Pista(setNombrePista=prompt("ingrese nombre de la pista: "),setDuracion=parseInt(prompt("ingrese duracion en minutos")));
+
+// disco.agregarPista(pista)
+
+let disco= new Disco();
+
+let pista = new Pista();
+
+let lista_discos= [];
